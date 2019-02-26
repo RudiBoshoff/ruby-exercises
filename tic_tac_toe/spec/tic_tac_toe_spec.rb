790bb9e4bc61057b7773a48a345bccc4aa1793af
Rudi Boshoff <30492583+RudiBoshoff@users.noreply.github.com>
2019-02-26 13:14:53 +0200

    Update tic_tac_toe_spec.rb
    Generally language fixes

--- tic_tac_toe/spec/tic_tac_toe_spec.rb
+++ tic_tac_toe/spec/tic_tac_toe_spec.rb
@@ -1,7 +1,7 @@
 require './tic_tac_toe'
 
 # TODO:
-# test to see if borad generated correctly
+# test to see if board generated correctly
 
 # test to see if turns alternate
 
@@ -10,10 +10,10 @@ require './tic_tac_toe'
 # test to see if move valid
 
 # test to check win conditions
-    # 1 three in a row horizontal
-    # 1 three in a row vertical
-    # 1 three in a row diagonal
-    # no three in a row (draw/ tie)
+    # 1. three in a row horizontal
+    # 2. three in a row vertical
+    # 3. three in a row diagonal
+    # 4. no three in a row (draw/ tie)
 
 # test to ensure game over
 
