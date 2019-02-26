7e40d709daf1338b0edb42d06973267efa8750be
Rudi Boshoff <rudiboshoff15@gmail.com>
2019-02-26 11:49:13 +0200

    Docs: adds test planning and documentation formatting

---
+++ tic_tac_toe/spec/tic_tac_toe_spec.rb
@@ -0,0 +1,16 @@
+require './tic_tac_toe'
+
+# TODO:
+# test to see if turns alternate
+
+# test to check if input is valid
+
+# test to see if move valid
+
+# test to check win conditions
+    # 1 three in a row horizontal
+    # 1 three in a row vertical
+    # 1 three in a row diagonal
+    # no three in a row
+
+# test for game restart
