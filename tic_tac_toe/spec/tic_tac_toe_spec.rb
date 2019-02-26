01b7e11621e4e53435c6a315b760d60d4fc911bd
Rudi Boshoff <rudiboshoff15@gmail.com>
2019-02-26 11:53:44 +0200

    Docs: additional tests planned

--- tic_tac_toe/spec/tic_tac_toe_spec.rb
+++ tic_tac_toe/spec/tic_tac_toe_spec.rb
@@ -1,6 +1,8 @@
 require './tic_tac_toe'
 
 # TODO:
+# test to see if borad generated correctly
+
 # test to see if turns alternate
 
 # test to check if input is valid
@@ -11,6 +13,8 @@ require './tic_tac_toe'
     # 1 three in a row horizontal
     # 1 three in a row vertical
     # 1 three in a row diagonal
-    # no three in a row
+    # no three in a row (draw/ tie)
+
+# test to ensure game over
 
 # test for game restart
